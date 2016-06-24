a85beca2df66d31d91351f47c991d3b80d8d4a13
arcynum <arcynum@gmail.com>
2016-06-24 17:54:09 +1000

    Managed to solve the routing issue with some creative reflection. The arguments come through as Strings, but that is because the regex matcher gives you them as strings. They can easily be parsed into Integers though.

--- src/au/com/ifti/Route.java
+++ src/au/com/ifti/Route.java
@@ -1,5 +1,6 @@
 package au.com.ifti;
 
+import java.lang.reflect.Method;
 import java.util.List;
 import java.util.regex.Pattern;
 
@@ -8,9 +9,9 @@ public class Route {
   private Pattern pattern = null;
   private List<String> httpMethods = null;
   private Class<?> controller = null;
-  private String method = null;
+  private Method method = null;
   
-  public Route(Pattern pattern, List<String> httpMethods, Class<?> controller, String method) {
+  public Route(Pattern pattern, List<String> httpMethods, Class<?> controller, Method method) {
     this.setPattern(pattern);
     this.setHttpMethods(httpMethods);
     this.setController(controller);
@@ -33,11 +34,11 @@ public class Route {
     this.controller = controller;
   }
 
-  public String getMethod() {
+  public Method getMethod() {
     return method;
   }
 
-  public void setMethod(String method) {
+  public void setMethod(Method method) {
     this.method = method;
   }
 
--- src/au/com/ifti/UrlDispatcher.java
+++ src/au/com/ifti/UrlDispatcher.java
@@ -19,103 +19,89 @@ import au.com.ifti.utilities.TiramisuRequest;
 import au.com.ifti.utilities.TiramisuResponse;
 
 public class UrlDispatcher {
-  
+
   private TiramisuRequest request;
   private TiramisuResponse response;
-  
+
   /**
    * This map is a regex expression pattern to the class to load next.
    */
   private List<Route> routes = new ArrayList<>();
-  
+
   /**
-   * Dispatching constructor, currently building the route list by hand in this function.
-   * Need to ensure the velocity engine is initialised.
+   * Dispatching constructor, currently building the route list by hand in this function. Need to
+   * ensure the velocity engine is initialised.
    */
-  public UrlDispatcher(HttpServletRequest servletRequest, HttpServletResponse servletResponse, Session session) {
+  public UrlDispatcher(HttpServletRequest servletRequest, HttpServletResponse servletResponse,
+      Session session) {
     this.request = new TiramisuRequest(servletRequest);
     this.response = new TiramisuResponse(servletResponse);
-    
+
     // Set the session in the response object.
     this.response.setSession(session);
 
-    
-    routes.add(new Route(Pattern.compile("^.*/reports[/]?"), Arrays.asList("GET"), ReportController.class, "index"));
-    routes.add(new Route(Pattern.compile("^.*/reports/([0-9]{1,})[/]?"), Arrays.asList("GET"), ReportController.class, "view"));
+    try {
+      routes.add(new Route(Pattern.compile("^.*/reports[/]?"), Arrays.asList("GET"),
+          ReportController.class, ReportController.class.getDeclaredMethod("index")));
+      routes.add(new Route(Pattern.compile("^.*/reports/([0-9]{1,})[/]?"), Arrays.asList("GET"),
+          ReportController.class, ReportController.class.getDeclaredMethod("view", String.class)));
+    } catch (NoSuchMethodException | SecurityException e) {
+      e.printStackTrace();
+    }
 
   }
 
   /**
    * The dispatch function called by the servlet to start the routing process.
+   * 
    * @param request
    * @param response
    */
   public void dispatch() {
-    
     Boolean matched = false;
-    
-    System.out.println(this.request.getRequestUri());
-    System.out.println(this.request.getMethod());
-    
+
+    // Loop through the routes looking for a match.
     for (Route route : routes) {
       Matcher m = route.getPattern().matcher(this.request.getRequestUri());
       // If the URL pattern matches.
       if (m.matches()) {
-        
+
         // If the HTTP method matches.
         if (route.getHttpMethods().contains(this.request.getMethod())) {
           matched = true;
-          
-          System.out.println(String.format("Matched on: %s over %s", m.group(0), this.request.getMethod()));
+
+          System.out.println(
+              String.format("Matched on: %s over %s", m.group(0), this.request.getMethod()));
           try {
-            
+
             // Create the controller (constructor).
-            Object controller = route.getController().getDeclaredConstructor(TiramisuRequest.class, TiramisuResponse.class).newInstance(this.request, this.response);
-            
-            String[] arguments = new String[m.groupCount()];
-            for (int i = 1; i < m.groupCount(); i++) {
-              arguments[i] = m.group(i);
+            Object controller = route.getController()
+                .getDeclaredConstructor(TiramisuRequest.class, TiramisuResponse.class)
+                .newInstance(this.request, this.response);
+
+            // Get the arguments.
+            Object[] arguments = new String[m.groupCount()];
+            for (int i = 0; i < m.groupCount(); i++) {
+              arguments[i] = m.group(i + 1);
             }
-            
-            switch (m.groupCount()) {
-              case 0: {
-                controller.getClass().getDeclaredMethod(route.getMethod()).invoke(controller);
-                break;
-              }
-              case 1: {
-                controller.getClass().getDeclaredMethod(route.getMethod(), String.class).invoke(controller, m.group(1));
-                break;
-              }
-              case 2: {
-                controller.getClass().getDeclaredMethod(route.getMethod(), String.class, String.class).invoke(controller, m.group(1), m.group(2));
-                break;
-              }
-              case 3: {
-                controller.getClass().getDeclaredMethod(route.getMethod(), String.class, String.class, String.class).invoke(controller, m.group(1), m.group(2), m.group(3));
-                break;
-              }
-              case 4: {
-                controller.getClass().getDeclaredMethod(route.getMethod(), String.class, String.class, String.class, String.class).invoke(controller, m.group(1), m.group(2), m.group(3), m.group(4));
-                break;
-              }
-            }
-            
-          }
-          catch (InstantiationException | IllegalAccessException | NoSuchMethodException | SecurityException | IllegalArgumentException e) {
+
+            // Invoke the function.
+            route.getMethod().invoke(controller, arguments);
+          } catch (InstantiationException | IllegalAccessException | NoSuchMethodException
+              | SecurityException | IllegalArgumentException e) {
             e.printStackTrace();
-          }
-          catch (InvocationTargetException e) {
+          } catch (InvocationTargetException e) {
             System.out.println(e);
             Throwable cause = e.getCause();
             System.out.println(cause);
             if (cause instanceof BadRequestException) {
-          	System.out.println("Bad request exception");
+              System.out.println("Bad request exception");
               this.response.setStatusCode(400);
               this.response.setTemplate("400.vm");
               this.response.setPageTitle("400");
             }
             if (cause instanceof NotFoundException) {
-          	System.out.println("Not found exception");
+              System.out.println("Not found exception");
               this.response.setStatusCode(404);
               this.response.setTemplate("404.vm");
               this.response.setPageTitle("404");
@@ -125,14 +111,14 @@ public class UrlDispatcher {
         }
       }
     }
-    
+
     // Nothing matched, 404.
     if (!matched) {
       this.response.setStatusCode(404);
       this.response.setTemplate("404.vm");
       this.response.setPageTitle("404");
     }
-    
+
   }
 
   public TiramisuRequest getRequest() {
@@ -142,11 +128,11 @@ public class UrlDispatcher {
   public TiramisuResponse getResponse() {
     return response;
   }
-  
+
   public void setRequest(TiramisuRequest request) {
     this.request = request;
   }
-  
+
   public void setResponse(TiramisuResponse response) {
     this.response = response;
   }
