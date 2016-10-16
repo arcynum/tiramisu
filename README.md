64c273de24f59a5c860429ce5d12ecff25f3565e
Chris Hamilton <arcynum@gmail.com>
2016-10-17 09:37:44 +1000

    More relevant README and more javadocs in the Controller.

--- README.md
+++ README.md
@@ -1,18 +1,10 @@
 # Tiramisu Web Application Framework
-#### A web application framework inspired by CakePHP 2.
 
 ---
 
-Because of the restrictions working within corporate environments, I have a need for a web application framework which is java based.
-In previous applications, I have used CakePHP for rapid web application development.
-I really like some of the design choices which the CakePHP project has used, so I have attempted to build a simple Java based web application which is similar to Cake.
+This is a web application framework written entirely in Java, using common libraries available to Java projects.
+The framework supports Regex URL patterns, a [Hibernate Database](http://docs.jboss.org/hibernate/orm/5.2/javadocs/org/hibernate/dialect/package-summary.html), velocity templates and everything in between. The framework is built around the MVC methodology.
 
-##### Rest API Notes ####
-After running into the well documented issue of REST butting heads against HTML forms, I decided to build a pure REST compliant interface. This means that the forms will have to be submitted via javascript.
-Whether the web interface is built in a framework like angular is still up for debate.
+Current plan is to implement user authentication, authorization coming at a later date.
 
 **This is still a work in progress**
-
----
-
-Don't forget to add the static directory serving, otherwise the web application won't serve static content.
--- src/au/com/ifti/controllers/Controller.java
+++ src/au/com/ifti/controllers/Controller.java
@@ -9,104 +9,155 @@ import au.com.ifti.models.Model;
 import au.com.ifti.utilities.TiramisuRequest;
 import au.com.ifti.utilities.TiramisuResponse;
 
+/**
+ * This is the abstract controller for the entire application. All controllers
+ * need to extend this controller.
+ * 
+ * @author CIH221
+ */
 public abstract class Controller {
 
-  protected TiramisuRequest request;
-  protected TiramisuResponse response;
-  protected Session session;
-  
-  public Controller(TiramisuRequest request, TiramisuResponse response, Session session) {
-    this.setRequest(request);
-    this.setResponse(response);
-    this.setSession(session);
-  }
+	/**
+	 * The application wrapped HTTP request object.
+	 */
+	protected TiramisuRequest request;
+	
+	/**
+	 * The application wrapped HTTP response object.
+	 */
+	protected TiramisuResponse response;
+	
+	/**
+	 * The session object, which will be passed in from the dispatcher.
+	 */
+	protected Session session = null;
 
-  public Session getSession() {
-    return session;
-  }
+	/**
+	 * The default constructor for the Controller objects.
+	 * @param request
+	 * @param response
+	 * @param session
+	 */
+	public Controller(TiramisuRequest request, TiramisuResponse response, Session session) {
+		this.setRequest(request);
+		this.setResponse(response);
+		this.setSession(session);
+	}
 
-  public void setSession(Session session) {
-    this.session = session;
-  }
+	/**
+	 * Find all the objects in the database of the specified type/class.
+	 * @param classIdentifier The object class you want returned.
+	 * @return Returns a casted List of objects returned from the database.
+	 */
+	public List<?> findAll(Class<?> classIdentifier) {
+		this.getSession().beginTransaction();
+		List<?> result = this.getSession().createCriteria(classIdentifier).list();
+		this.getSession().getTransaction().commit();
+		return result;
+	}
 
-  public TiramisuRequest getRequest() {
-    return request;
-  }
+	/**
+	 * A type casted object, of the specified id, from the database.
+	 * @param classIdentifier The class of the object you are requesting.
+	 * @param id The primary key of the object you are requesting.
+	 * @return A single object of the type passed into the function.
+	 */
+	public <T extends Model> T findById(Class<T> classIdentifier, Integer id) {
+		this.getSession().beginTransaction();
+		Object result = this.getSession().get(classIdentifier, id);
+		this.getSession().getTransaction().commit();
 
-  public TiramisuResponse getResponse() {
-    return response;
-  }
+		try {
+			return classIdentifier.cast(result);
+		} catch (ClassCastException e) {
+			return null;
+		}
+	}
 
-  public void setRequest(TiramisuRequest request) {
-    this.request = request;
-  }
+	/**
+	 * Save the object to the database and updated the timestamps.
+	 * @param object A object which extends from the Model class.
+	 * @return Whether the object saved or not.
+	 */
+	public Boolean save(Model object) {
+		this.getSession().beginTransaction();
+		// So the timestamps match. Create a single object.
+		Date now = new Date();
+		object.setCreated(now);
+		object.setModified(now);
+		this.getSession().save(object);
+		this.getSession().getTransaction().commit();
+		return true;
+	}
 
-  public void setResponse(TiramisuResponse response) {
-    this.response = response;
-  }
-  
-  public List<?> findAll(Class<?> classIdentifier) {
-    this.getSession().beginTransaction();
-    List<?> result = this.getSession().createCriteria(classIdentifier).list();
-    this.getSession().getTransaction().commit();
-    return result;
-  }
+	/**
+	 * Update an object in the database.
+	 * @param object A object which extends from the Model class.
+	 * @return Whether the object updated or not.
+	 */
+	public Boolean update(Model object) {
+		this.getSession().beginTransaction();
+		object.setModified(new Date());
+		this.getSession().save(object);
+		this.getSession().getTransaction().commit();
+		return true;
+	}
 
-  public <T extends Model> T findById(Class<T> classIdentifier, Integer id) {
-    this.getSession().beginTransaction();
-    Object result = this.getSession().get(classIdentifier, id);
-    this.getSession().getTransaction().commit();
+	/**
+	 * Add a view variable to the response object.
+	 * View variables will be available to be output in the velocity template.
+	 * @param key The view variable key. Make sure this is unique.
+	 * @param value The value you want to set. This can be any valid Java object.
+	 */
+	public void set(String key, Object value) {
+		this.getResponse().addViewVariable(key, value);
+	}
 
-    try {
-      return classIdentifier.cast(result);
-    } catch (ClassCastException e) {
-      return null;
-    }
-  }
-  
-  public Boolean save(Model object) {
-    this.getSession().beginTransaction();
-    // So the timestamps match.
-    Date now = new Date();
-    object.setCreated(now);
-    object.setModified(now);
-    this.getSession().save(object);
-    this.getSession().getTransaction().commit();
-    return true;
-  }
-  
-  public Boolean update(Model object) {
-    this.getSession().beginTransaction();
-    object.setModified(new Date());
-    this.getSession().save(object);
-    this.getSession().getTransaction().commit();
-    return true;
-  }
-  
-  public void set(String key, Object value) {
-    this.getResponse().addViewVariable(key, value);
-  }
-  
-  public TiramisuResponse redirect(String url) {
-    
-    // Set the status code
-    this.getResponse().setStatusCode(303);
-    
-    // Set the location header.
-    this.getResponse().setHeader("Location", url);
-    
-    return this.getResponse();
-  }
-  
-  public TiramisuResponse redirect(String url, Integer code) {
-    
-    // Set the status code
-    this.getResponse().setStatusCode(code);
-    
-    // Set the location header.
-    this.getResponse().setHeader("Location", url);
-    
-    return this.getResponse();
-  }
+	/**
+	 * Redirect to the provided URL.
+	 * @param url Where to redirect to.
+	 * @return The TiramisuResponse object.
+	 */
+	public TiramisuResponse redirect(String url) {
+		this.getResponse().setStatusCode(303);
+		this.getResponse().setHeader("Location", url);
+		return this.getResponse();
+	}
 
+	/**
+	 * Redirect to the provided URL with a custom HTTP code.
+	 * @param url Where to redirect to.
+	 * @param code The HTTP code you want to present.
+	 * @return The TiramisuResponse object.
+	 */
+	public TiramisuResponse redirect(String url, Integer code) {
+		this.getResponse().setStatusCode(code);
+		this.getResponse().setHeader("Location", url);
+		return this.getResponse();
+	}
+	
+	public Session getSession() {
+		return session;
+	}
+
+	public void setSession(Session session) {
+		this.session = session;
+	}
+
+	public TiramisuRequest getRequest() {
+		return request;
+	}
+
+	public TiramisuResponse getResponse() {
+		return response;
+	}
+
+	public void setRequest(TiramisuRequest request) {
+		this.request = request;
+	}
+
+	public void setResponse(TiramisuResponse response) {
+		this.response = response;
+	}
+	
 }
